b2a8fc4a2e7b25e0354a4ee31093131f83156963
Elitelord <sameeragarwal007@outlook.com>
2025-10-30 23:05:06 -0500

    adding localHost

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ label_encoder = joblib.load("models/plan_label_encoder.joblib")
 # --- Enable CORS ---
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://carta-ecru.vercel.app/"],
+    allow_origins=["https://carta-ecru.vercel.app/", "http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
